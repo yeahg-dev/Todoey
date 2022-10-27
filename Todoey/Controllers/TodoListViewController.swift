d270116e04abc94893b48b0670335c2b53ec786a
yeahg-dev <yeahg.dev@gmail.com>
2022-10-28 00:00:02 +0900

    FEAT: Item 타입 모델링, Documents를 Storage로 구현

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    var items :[Item] = []
 
-    var items = ["take a shower", "buy lunch", "go running"]
+    let dataFilePath = FileManager.default
+        .urls(for: .documentDirectory, in: .userDomainMask)
+        .first?
+        .appendingPathExtension("ItemArray")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadItems()
         navigationController?.navigationBar.barStyle = .black
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = UIColor(red: 0.448, green: 0.766, blue: 0.937, alpha: 1)
@@ -26,7 +32,7 @@ class TodoListViewController: UITableViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-
+    
     // MARK: - TableView DataSource
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -45,17 +51,20 @@ class TodoListViewController: UITableViewController {
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        let item = items[indexPath.row]
+        
         if #available(iOS 14.0, *) {
             var configuration = cell.defaultContentConfiguration()
-            configuration.text = items[indexPath.row]
+            configuration.text = item.title
             cell.contentConfiguration = configuration
         } else {
-            cell.textLabel?.text = items[indexPath.row]
+            cell.textLabel?.text = item.title
         }
-                
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
-
+    
     // MARK: - TableView Delegate
     
     override func tableView(
@@ -63,13 +72,10 @@ class TodoListViewController: UITableViewController {
         didSelectRowAt indexPath: IndexPath
     ) {
         let cell = tableView.cellForRow(at: indexPath)
-        if cell?.accessoryType == .checkmark {
-            cell?.accessoryType = .none
-        } else {
-            cell?.accessoryType = .checkmark
-        }
-    
+        items[indexPath.row].done = !items[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
+        saveItems()
     }
     
     // MARK: - Add New Item
@@ -81,15 +87,38 @@ class TodoListViewController: UITableViewController {
         }
         
         let addAction = UIAlertAction(title: "추가", style: .default) { _ in
-            if let item = alert.textFields?.first?.text,
-               !item.isEmpty {
-                self.items.append(item)
+            if let title = alert.textFields?.first?.text,
+               !title.isEmpty {
+                let newItem  = Item(title: title)
+                self.items.append(newItem)
                 self.tableView.reloadData()
+                self.saveItems()
             }
         }
         alert.addAction(addAction)
         
         present(alert, animated: true)
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                items = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding Item Array \(error)")
+            }
+        }
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.items)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("encoding error \(error)")
+        }
     }
     
 }
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Moon Yeji on 2022/10/27.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Item: Codable {
+    
+    var title: String
+    var done: Bool = false
+    
+}
